fd1cd0890831187f26c6f96f873996fbf0224601
phucThe <109821787+phucThe@users.noreply.github.com>
2022-07-24 21:58:18 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,11 +7,16 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'UNIMART') }}</title>
 
     <!-- Scripts -->
+    <script src="{{ asset('js/jquery-3.6.0.min.js') }}" type="text/javascript"></script>
     <script src="{{ asset('js/app.js') }}"></script>
-    <!-- @vite(['resources/js/app.js']) -->
+    
+    {{-- @vite(['resources/js/app.js']) --}}
+    
+    <!-- Font awesome -->
+    <link rel="stylesheet" href="{{ asset('plugin/fontawesome/css/all.css') }}">
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
